d541e25bc353ebb6201440d518c68f30cbdfa5d1
ayrton-hbtn <3282@holbertonschool.com>
2022-04-11 17:06:09 -0300

    pascal triangle

---
+++ 0-pascal_triangle.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Pascal's Triangle """
+
+
+def pascal_triangle(n):
+    """
+    returns a Pascal's Triangle
+    of size 'n'
+    """
+    triangle = [[1]]
+    prev = []
+    row = [1]
+    current = 0
+
+    while n >= 2:
+        prev = triangle[current]
+        for i in range(len(prev)):
+            try:
+                row.append(prev[i] + prev[i + 1])
+            except IndexError:
+                row.append(1)
+        triangle.append(row)
+        row = [1]
+        current += 1
+        n -= 1
+
+    return triangle
