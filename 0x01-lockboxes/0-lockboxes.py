282dbd891057a27a03025304bed9cdef9b6c7b27
ayrton-hbtn <3282@holbertonschool.com>
2022-04-18 18:03:31 -0300

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+""" Lockboxes """
+
+
+def canUnlockAll(boxes):
+    """
+    Iterate over a list of boxes with keys inside.
+    Each box may contain keys to other boxes, a key
+    with the same number as the index of a box can
+    open that box. Determine if all boxes can be opened.
+    """
+    n = len(boxes)
+    keys = []  # create list of index keys
+    for i in range(n):
+        keys.append(0)  # default value 0 (no key)
+    keys[0] = 1  # first box is opened
+
+    for i in range(len(boxes)):  # iterate over boxes
+        if keys[i] == 1:  # open box if we have the key
+            for key in boxes[i]:  # for each key inside that box
+                try:
+                    keys[key] = 1  # get the key
+                except KeyError:
+                    pass
+        for i in range(len(boxes)):  # iterate once more to open previous boxes
+            if keys[i] == 1:
+                for key in boxes[i]:
+                    try:
+                        keys[key] = 1
+                    except KeyError:
+                        pass
+
+    for key in keys:  # check if we found all keys
+        if key == 0:
+            return False  # return false if one key is missing
+    return True  # else return true
