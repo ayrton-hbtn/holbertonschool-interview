32beb2e633e72ff7109162217109302c601ea006
ayrton-hbtn <3282@holbertonschool.com>
2022-04-20 09:47:41 -0300

    case for no boxes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -10,19 +10,21 @@ def canUnlockAll(boxes):
     open that box. Determine if all boxes can be opened.
     """
     n = len(boxes)
+    if not n:
+        return False
     keys = []  # create list of index keys
     for i in range(n):
         keys.append(0)  # default value 0 (no key)
     keys[0] = 1  # first box is opened
 
-    for i in range(len(boxes)):  # iterate over boxes
+    for i in range(n):  # iterate over boxes
         if keys[i] == 1:  # open box if we have the key
             for key in boxes[i]:  # for each key inside that box
                 try:
                     keys[key] = 1  # get the key
                 except KeyError:
                     pass
-        for i in range(len(boxes)):  # iterate once more to open previous boxes
+        for i in range(n):  # iterate once more to open previous boxes
             if keys[i] == 1:
                 for key in boxes[i]:
                     try:
