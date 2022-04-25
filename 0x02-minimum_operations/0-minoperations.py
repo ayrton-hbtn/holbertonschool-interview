91b033a8a195fb9b62f23c5b6c9bfc826a5e96ef
ayrton-hbtn <3282@holbertonschool.com>
2022-04-25 18:28:14 -0300

    add corner cases n=1, n=2

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,12 +8,17 @@ def minOperations(n):
     needed to result in exactly n H characters in the file
     using only copy and paste operations
     """
+    if n == 1:
+        return 0
     h = 'H'
     copy = h
     num_operations = 0
     while len(h) < n:
         if len(h + copy) >= n:
-            return num_operations + 1
+            if len(h) == 1:
+                return num_operations + 2
+            else:
+                return num_operations + 1
         if n % 2 == 1 and n % 3 == 0 and len(h) == 2:
             h += copy
             num_operations += 1
