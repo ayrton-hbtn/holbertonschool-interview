bbfbc1305ae0f59150ef26421e9a25ec4686538f
ayrton-hbtn <3282@holbertonschool.com>
2022-04-27 03:31:48 -0300

    and yet another try

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -15,15 +15,17 @@ def minOperations(n: int) -> int:
     h = 'H'
     copy = h[::]
     min_operations = 1
-    for i in range(1, n + 1):
+    for i in range(2, n + 1):
         if len(h) >= n:
             break
         if len(h + copy) >= n:
             return min_operations + 1
-        else:
-            if len(h) >= ceil(n / 2) or len(h) == 3:
-                copy = h[::]
-                min_operations += 1
+        if len(h) == 3:
+            copy = h[::]
+            min_operations += 1
+        if len(h) == i / 2 and len(copy) < len(h) / 2:
+            copy = h[::]
+            min_operations += 1
         h += copy
         min_operations += 1
 
