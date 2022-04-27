ebfa5cfda42270b0ae735a51fe11f4627398c321
ayrton-hbtn <3282@holbertonschool.com>
2022-04-27 03:49:13 -0300

    done

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -8,26 +8,10 @@ def minOperations(n: int) -> int:
     needed to result in exactly n H characters in the file
     using only copy and paste operations
     """
-    if n == 1:
-        return 0
-    h = 'H'
-    copy = h
-    num_operations = 0
-    while len(h) < n:
-        if len(h + copy) >= n:
-            if len(h) == 1:
-                return num_operations + 2
-            else:
-                return num_operations + 1
-        if n % 2 == 1 and n % 3 == 0 and len(h) % 2 == 0:
-            h += copy
-            num_operations += 1
-            continue
-        elif len(h + copy) >= n - len(copy):
-            h += copy
-            num_operations += 1
-            continue
-        copy = h
-        h += copy
-        num_operations += 2
-    return
+    operations = 0
+    for i in range(2, n + 1):
+        while n % i == 0:
+            operations += i
+            n = n / i
+
+    return operations
