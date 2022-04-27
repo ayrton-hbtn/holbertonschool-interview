b5f581ad20160c034f7d64797beb1500a98ae89a
ayrton-hbtn <3282@holbertonschool.com>
2022-04-27 03:03:25 -0300

    another

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Minimum operations"""
 
+from math import ceil
+
 
 def minOperations(n: int) -> int:
     """
@@ -8,30 +10,21 @@ def minOperations(n: int) -> int:
     needed to result in exactly n H characters in the file
     using only copy and paste operations
     """
-    first_cases = {
-        1: 0,
-        2: 2,
-        3: 3,
-        4: 4,
-        5: 5,
-        6: 5
-    }
-    for n_value, op in first_cases.items():
-        if n_value == n:
-            return op
-    h = 'HHHHHH'
-    copy = 'HHH'
-    min_operations = 5
-    for i in range(7, n + 1):
+    if n == 1:
+        return 0
+    h = 'H'
+    copy = h[::]
+    min_operations = 1
+    for i in range(1, n + 1):
+        if len(h) >= n:
+            break
         if len(h + copy) >= n:
             return min_operations + 1
         else:
-            if len(h) >= i / 2:
+            if len(h) >= ceil(n / 2) or len(h) == 3:
                 copy = h[::]
                 min_operations += 1
         h += copy
         min_operations += 1
-        if len(h) >= n:
-            break
 
     return min_operations
