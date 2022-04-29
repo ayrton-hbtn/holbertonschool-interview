719ec6d722cf4de599586d1b4019089f8ff5801f
ayrton-hbtn <3282@holbertonschool.com>
2022-04-28 23:34:07 -0300

    fix

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -5,13 +5,17 @@
 def minOperations(n: int) -> int:
     """
     Given n, calculate the fewest number of operations
-    needed to result in exactly n H characters in the file
-    using only copy and paste operations
+    needed to result in exactly n H characters using
+    only copy and paste operations
     """
     operations = 0
-    for i in range(2, n + 1):
-        while n % i == 0:
+    i = 2
+    while i <= n + 1:
+        if n % i == 0:
             operations += i
             n = n / i
+            i = 2
+        else:
+            i += 1
 
     return operations
