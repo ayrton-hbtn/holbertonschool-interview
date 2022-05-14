43d450470f742d59dc61f314b4d43d7d0712505a
ayrton-hbtn <3282@holbertonschool.com>
2022-05-13 21:43:07 -0300

    first byte check the 8 least significant bits

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -14,19 +14,17 @@ def validUTF8(data):
     number of bytes that the character will have.
     """
     num_bytes = 0
+    mask = 255
 
     for byte in data:
         if num_bytes == 0:
+            byte = byte & mask
             if (byte >> 5) == 0b110:
                 num_bytes = 1
             elif (byte >> 4) == 0b1110:
                 num_bytes = 2
             elif (byte >> 3) == 0b11110:
                 num_bytes = 3
-            elif (byte >> 2) == 0b111110:
-                return False
-            elif (byte >> 1) == 0b1111110:
-                return False
             elif (byte >> 7):
                 return False
         else:
