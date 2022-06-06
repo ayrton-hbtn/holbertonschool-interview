c1ae4123ac433898eb73f1dcc2be89920f892065
ayrton-hbtn <3282@holbertonschool.com>
2022-06-06 01:26:41 -0300

    making change

---
+++ 0x08-making_change/0-making_change.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Making Change"""
+
+
+def makeChange(coins, total):
+    """
+    Given a pile of coins of different values,
+    determine the fewest number of coins needed
+    to meet a given amount total
+    """
+    coins.sort(reverse=True)
+    count = 0
+    change = 0
+    for coin in coins:
+        if change + coin > total:
+            continue
+        while change + coin <= total:
+            change += coin
+            count += 1
+        if change == total:
+            break
+
+    if change != total:
+        return -1
+    return count
